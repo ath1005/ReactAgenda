1e2c4e5745e77b7d613620f98de7cdc091a61b19
Atlee Hasson <60123127+ath1005@users.noreply.github.com>
2020-09-16 13:25:20 -0400

    Added a task array in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskForm from "./TaskForm";
 import "/src/styles.css";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(task) {
+    setTasks((prevTasks) => {
+      return [...prevTasks, task];
+    });
+  }
+
   return (
     <div className="App">
       <h1>Agenda</h1>
-      <TaskForm />
+      <TaskForm onAdd={addTask} />
     </div>
   );
 }
