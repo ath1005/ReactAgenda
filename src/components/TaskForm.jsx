e8bed72b3efb78a8a74f6391c1e5f0d489781664
Atlee Hasson <60123127+ath1005@users.noreply.github.com>
2020-09-16 13:35:48 -0400

    Gave TaskForm the functionality to add a task to the task array in App

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,16 +3,53 @@ import React, { useState } from "react";
 function TaskForm(props) {
   const [task, setTask] = useState({
     title: "",
-    date: ""
+    date: "",
+    comments: ""
   });
+
+  function handleInput(event) {
+    const { value, name } = event.target;
+
+    setTask((prevTask) => {
+      return {
+        ...prevTask,
+        [name]: value
+      };
+    });
+  }
+
+  function handleAdd(event) {
+    props.onAdd(task);
+    setTask({
+      title: "",
+      date: "",
+      comments: ""
+    });
+    event.preventDefault();
+  }
 
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <input name="date" placeholder="Date" />
-        <textarea name="comments" placeholder="Comments" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleInput}
+          value={task.title}
+        />
+        <input
+          name="date"
+          placeholder="Date"
+          onChange={handleInput}
+          value={task.date}
+        />
+        <textarea
+          name="comments"
+          placeholder="Comments"
+          onChange={handleInput}
+          value={task.comments}
+        />
+        <button onClick={handleAdd}>Add</button>
       </form>
     </div>
   );
