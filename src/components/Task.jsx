5f6c07414190f9fa786ba1397bc59f421ac63eca
Atlee Hasson <60123127+ath1005@users.noreply.github.com>
2020-09-20 18:38:10 -0400

    Adjusted the display of information and styling for Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,8 +4,8 @@ import DoneIcon from "@material-ui/icons/Done";
 function Task(props) {
   return (
     <div className="task">
+      <h5>{props.date}</h5>
       <h1>{props.title}</h1>
-      <h2>{props.date}</h2>
       <p>{props.comments}</p>
       <button onClick={() => props.onDelete(props.id)}>
         <DoneIcon />
